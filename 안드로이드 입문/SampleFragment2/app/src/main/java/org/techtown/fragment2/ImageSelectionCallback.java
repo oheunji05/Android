8ea28f56ef9e739c097fad6a812bc3e556caac08
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-03-16 20:19:52 +0900

    [ADD] FILE

---
+++ 안드로이드 입문/SampleFragment2/app/src/main/java/org/techtown/fragment2/ImageSelectionCallback.java
@@ -0,0 +1,5 @@
+package org.techtown.fragment2;
+
+public interface ImageSelectionCallback {
+    public void onImageSelected(int position); //몇 번째 이미지가 선택되었는지
+}
---
+++ 안드로이드 입문/SampleFragment2/app/src/main/java/org/techtown/fragment2/ListFragment.java
@@ -0,0 +1,66 @@
+package org.techtown.fragment2;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+    ImageSelectionCallback callback;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        //
+        if(context instanceof  ImageSelectionCallback){
+            callback = (ImageSelectionCallback) context;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_list,container,false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null){
+                    callback.onImageSelected(1);
+                }
+            }
+        });
+
+        Button button2 = rootView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null){
+                    callback.onImageSelected(2);
+                }
+            }
+        });
+
+        Button button3 = rootView.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null){
+                    callback.onImageSelected(3);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+}
